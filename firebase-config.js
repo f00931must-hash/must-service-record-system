9b5229b4dbca5ca0e168d0f8f0339bc7f213c4e1
f00931must-hash <f00931.must@gmail.com>
2026-07-14 11:39:39 +0800

    Add files via upload

--- firebase-config.js
+++ firebase-config.js
@@ -1,11 +1,11 @@
 // 請把 Firebase 專案的 Web App 設定貼在這裡。
 export const firebaseConfig = {
-  apiKey: "請貼上",
-  authDomain: "請貼上",
-  projectId: "請貼上",
-  storageBucket: "請貼上",
-  messagingSenderId: "請貼上",
-  appId: "請貼上"
+  apiKey: "AIzaSyB3rVyZb9VzDgQ90CQmLF_JXlM-wDskGxE",
+  authDomain: "must-service-record-system.firebaseapp.com",
+  projectId: "must-service-record-system",
+  storageBucket: "must-service-record-system.firebasestorage.app",
+  messagingSenderId: "1066337913135",
+  appId: "1:1066337913135:web:3080eb1e05b176de77b9da"
 };
 
 // 可選：限制只有指定學校網域登入，例如 must.edu.tw。
